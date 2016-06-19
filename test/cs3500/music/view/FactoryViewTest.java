b457fefdf94e4cbd27d6ddeb9b227de2802d91cc
Brendan Rejevich <rejevich.b@husky.neu.edu>
2016-06-18 23:07:09 -0400

    finished factory view tests

--- test/cs3500/music/view/FactoryViewTest.java
+++ test/cs3500/music/view/FactoryViewTest.java
@@ -1,5 +1,6 @@
 package cs3500.music.view;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,7 +12,21 @@ public class FactoryViewTest {
 
     @Test
     public void create() throws Exception {
+        IMusicPieceView console = FactoryView.create("console");
+        IMusicPieceView midi = FactoryView.create("midi");
+        IMusicPieceView visual = FactoryView.create("visual");
 
+        Assert.assertNotNull(console);
+        Assert.assertNotNull(midi);
+        Assert.assertNotNull(visual);
+
+        Assert.assertNotSame(console, midi);
+        Assert.assertNotSame(console, visual);
+        Assert.assertNotSame(midi, visual);
+
+        Assert.assertTrue(console instanceof ConsoleViewFrame);
+        Assert.assertTrue(midi instanceof MidiViewImpl);
+        Assert.assertTrue(visual instanceof GuiViewFrame);
     }
 
 }
